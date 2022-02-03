2bb8e806d5b1e163923cd2976279bf6f28585e64
ikyrie <mah.c5r@gmail.com>
2022-02-03 16:59:58 -0300

    Testando AsyncStorage

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('')
+
+  function storeData(value) {
+    try {
+      AsyncStorage.setItem('1', value)
+      console.log('Valor guardado: ' + value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function restoreData(key) {
+    try {
+      const value = await AsyncStorage.getItem(key.toString())
+      console.log(value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testando</Text>
+      <TextInput onChangeText={newText => setText(newText)} placeholder='Digite aqui algum texto'/>
+      <Button onPress={() => {storeData(text)}} title='Salvar'/>
+      <Button onPress={() => {restoreData('1')}} title='Mostrar'/>
       <StatusBar style="auto" />
     </View>
   );
